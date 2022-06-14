62f193c4165add213d5d5a640372db517fa78006
brayanpasa99 <brayanpasa99@hotmail.com>
2022-06-14 18:02:21 -0500

    devops: Devolver slice sin objetos
    Resuelve: udistrital/tesoreria_cliente#171

--- controllers/tipo_ingreso.go
+++ controllers/tipo_ingreso.go
@@ -134,7 +134,7 @@ func (c *TipoIngresoController) GetAll() {
 		c.Abort("404")
 	} else {
 		if l == nil {
-			l = append(l, map[string]interface{}{})
+			l = []interface{}{}
 		}
 		c.Data["json"] = map[string]interface{}{"Success": true, "Status": "200", "Message": "Request successful", "Data": l}
 	}
