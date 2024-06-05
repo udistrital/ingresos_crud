c72fe58979e867d2b357401e8a54d30291fa9b4e
JulianAvella <julianavella01@gmail.com>
2024-06-05 12:11:12 -0500

    feat: Terminar la instrumentalización de las APIs udistrital/core_documentacion#32

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
 	"github.com/udistrital/utils_oas/auditoria"
 	"github.com/udistrital/utils_oas/customerrorv2"
+	"github.com/udistrital/utils_oas/xray"
 )
 
 func main() {
@@ -42,6 +43,7 @@ func main() {
 
 	auditoria.InitMiddleware()
 	beego.ErrorController(&customerrorv2.CustomErrorController{})
+	xray.InitXRay()
 	apistatus.Init()
 	beego.Run()
 }
